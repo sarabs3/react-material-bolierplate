46fd6d79473f090df5cd75cd2fa676ebeccb47df
Sarabjeet Singh <sarabsingh212@gmail.com>
2018-05-19 17:23:29 +0530

    broken link fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
 import SimpleReducer from './simpleReducer';
-import ProfileReducer from './profileReducer';
 
 export default combineReducers({ SimpleReducer });
